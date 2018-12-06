08cb9243b80164296ce82b11df384bf847dbcece
DarkPillor <arthur.wolff@viacesi.fr>
2018-12-06 11:49:10 +0100

    1er commit

---
+++ Restaurant/Salle/Commande.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salle
+{
+    class Commande
+    {
+        private int id;
+        private string nomMenu;
+        private int prix;
+
+        public Commande(string nom, int prix)
+        {
+
+        }
+    }
+}
---
+++ Restaurant/Salle/MaitreHotel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salle
+{
+    class MaitreHotel
+    {
+        private string nom;
+        private string prenom;
+        private readonly MaitreHotel UniqueInstance;
+
+        public CheckClient()
+        {
+
+        }
+
+        public AppelIChefRang()
+        {
+
+        }
+
+        public VerifieTableDisponible()
+        {
+
+        }
+
+        public AttribueTable()
+        {
+
+        }
+        
+        public int GetPosition()
+        {
+
+        }
+
+        private MaitreHotel()
+        {
+
+        }
+
+        public MaitreHotel GetInstance()
+        {
+
+        }
+
+    }
+}
