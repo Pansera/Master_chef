26aeaa31a325c7c4ca482ac6a768633f40b001fb
Flo <florian.pfeifer@viacesi.fr>
2018-12-07 10:36:10 +0100

    Classe Controler

---
+++ Restaurant/Cuisine/Controler/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Cuisine.Controler
+{
+    class Client
+    {
+
+        public static Socket socket = null;
+        public static Thread t1;  
+        private const int port = 11000;
+
+        private static ManualResetEvent connectDone =
+            new ManualResetEvent(false);
+        private static ManualResetEvent sendDone =
+            new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone =
+            new ManualResetEvent(false);
+
+        // The response from the remote device.  
+        private static String response = String.Empty;
+
+
+        private static void StartClient()
+        {
+            // Connect to a remote device.  
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry("host.contoso.com");
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+
+                Socket client = new Socket(ipAddress.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
