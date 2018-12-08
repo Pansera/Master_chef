0bba32976a6cd3a5a0c7331b00f2e18be384a2dc
Pierre Marlier <pierre.marlier@viacesi.fr>
2018-12-08 12:10:33 +0100

    Ajout classe horloge

---
+++ Restaurant/Salle/Model/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace testThread
+{
+    public class Horloge
+    {
+        private static int compteur = 0;
+        private static Mutex mut = new Mutex();
+        private static bool stopHorloge = false;
+
+        public int Compteur { get => compteur; set => compteur = value; }
+        public static bool StopHorloge { get => stopHorloge; set => stopHorloge = value; }
+
+        public void Start()
+        {
+            StopHorloge = false;
+            Console.WriteLine("Création du thread :");
+            Thread t = new Thread(StartTemps);
+            t.Start();
+            Console.WriteLine("t Start");
+        }
+        public void StartTemps()
+        {
+            do
+            {
+                Compteur = Compteur + 1;
+                Console.WriteLine(Compteur + " seconde");
+                Thread.Sleep(1000);
+
+            } while (StopHorloge == false && Compteur < 20);
+        }
+
+        public void StopTemps()
+        {
+            StopHorloge = true;
+        }
+        public void reinitTemps()
+        {
+            Compteur = 0;
+        }
+        public class Programme
+        {
+            static void Main()
+            {
+                Horloge horloge = new Horloge();
+                horloge.Start();
+            }
+        }
+    }
+}
