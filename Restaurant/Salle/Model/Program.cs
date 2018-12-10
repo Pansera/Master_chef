2424cf6bf0f2a60a987d0d697e5a0f3a20973d40
filipelenoir <filipe.lenoirmaiaesilva@viacesi.fr>
2018-12-10 09:55:27 +0100

    (Erreur GH) modif main pour class table

--- Restaurant/Salle/Model/Program.cs
+++ Restaurant/Salle/Model/Program.cs
@@ -10,6 +10,16 @@ namespace Salle
     {
         static void Main(string[] args)
         {
+            List<Table> tables4p = new List<Table>();
+            List<Table> tables8p = new List<Table>();
+            for (int i = 0; i < 10; i++)
+            {
+                tables4p.Add(new Table() { Id = i, nbPlace = 4, Eau = false, Pain = false });
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                tables8p.Add(new Table() { Id = i, nbPlace = 8, Eau = false, Pain = false });
+            }
         }
     }
 }
