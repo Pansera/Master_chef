b8113430660f25018689a9cb9d249bd5442119e4
Pierre Marlier <pierre.marlier@viacesi.fr>
2018-12-12 15:06:15 +0100

    ajoute classe modèle

---
+++ Restaurant/Cuisine/Model/Poste/ChefCuisine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuisine.Model
+{
+    public sealed class ChefCuisine : APosteFixe
+    {
+        private int id;
+        private static ChefCuisine instance = null;
+        private static readonly object padlock = new object();
+        public int Id1 { get => id; set => id = value; }
+        ChefCuisine() { }
+
+        public static ChefCuisine Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ChefCuisine();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+
+        public void getID()
+        {
+            Id++;
+            Id1 = Id;
+        }
+
+        public void recoitCommande()
+        {
+
+        }
+
+        public void checkStock()
+        {
+
+        }
+    }
+}
---
+++ Restaurant/Cuisine/Model/Élément/LaveVaiselle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuisine.Model
+{
+    class LaveVaiselle
+    {
+        private bool isPlein;
+
+        public void Nettoie()
+        {
+
+        }
+    }
+}
